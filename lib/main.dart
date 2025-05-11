fe4c288fc099e3c8b3a24898a9334ac02dcec368
maramrmilad7 <maro12787@gmail.com>
2025-05-11 23:13:37 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ResponsiveDashBoard());
+}
+
+class ResponsiveDashBoard extends StatelessWidget {
+  const ResponsiveDashBoard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),);
+  }
+}
