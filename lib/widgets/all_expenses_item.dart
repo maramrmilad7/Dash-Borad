27d27eb5a4edb5ef1a50987fae28a1bdc918e4a6
maramrmilad7 <maro12787@gmail.com>
2025-05-25 18:01:28 +0200

    create all expenses model and display the all expenses header

--- lib/widgets/all_expenses_item.dart
+++ lib/widgets/all_expenses_item.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:responsiveui/models/all_expenses_item_model.dart';
+import 'package:responsiveui/widgets/all_expenses_item_header.dart';
 
 class AllExpensesItem extends StatelessWidget {
-  const AllExpensesItem({super.key});
+  const AllExpensesItem({super.key, required this.itemModel});
+  final AllExpensesItemModel itemModel;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+      decoration: ShapeDecoration(
+        color: Colors.white,
+        shape: RoundedRectangleBorder(
+          side: BorderSide(color: Color(0xFFF1F1F1), width: 1),
+          borderRadius: BorderRadius.circular(12),
+        ),
+      ),
+      child: Column(
+        children: [
+          AllExpensesItemHeader(
+            image: itemModel.image,
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/widgets/all_expenses_widget.dart
+++ lib/widgets/all_expenses_widget.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:responsiveui/models/all_expenses_item_model.dart';
+import 'package:responsiveui/utils/app_images.dart';
 import 'package:responsiveui/widgets/all_expenses_header.dart';
+import 'package:responsiveui/widgets/all_expenses_item.dart';
 
 class AllExpenses extends StatelessWidget {
   const AllExpenses({super.key});
@@ -9,18 +12,22 @@ class AllExpenses extends StatelessWidget {
     return Container(
       padding: const EdgeInsets.all(20),
       decoration: ShapeDecoration(
-        color: Colors.white,
-shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10),
-
-)  
-
-
-   ),
-   child: Column(children: [
-    AllExpensesHeader(),
-   ],),
-
+          color: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          )),
+      child: Column(
+        children: [
+          AllExpensesHeader(),
+          AllExpensesItem(
+              itemModel: AllExpensesItemModel(
+            image: Assets.imagesIncome,
+            title: 'Income',
+            price: r'/$120.00',
+            date: 'April 2025',
+          )),
+        ],
+      ),
     );
   }
 }
