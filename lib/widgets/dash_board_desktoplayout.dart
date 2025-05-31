d771346da16c82928e1e9b26ba75dd927bd9f9d8
maramrmilad7 <maro12787@gmail.com>
2025-06-01 00:15:12 +0200

    my card section done

--- lib/widgets/dash_board_desktoplayout.dart
+++ lib/widgets/dash_board_desktoplayout.dart
@@ -5,6 +5,7 @@ import 'package:responsiveui/widgets/all_expenses_widget.dart';
 import 'package:responsiveui/widgets/custom_drawer.dart';
 import 'package:responsiveui/widgets/my_card.dart';
 import 'package:responsiveui/widgets/my_card_page_view.dart';
+import 'package:responsiveui/widgets/my_cards_section.dart';
 
 class DashBoardDesktopLayout extends StatelessWidget {
   const DashBoardDesktopLayout({super.key});
@@ -20,7 +21,7 @@ class DashBoardDesktopLayout extends StatelessWidget {
           height: 24,
         ),
         Expanded(
-          child: MyCardPageView(),
+          child: MyCardsSection(),
         )
       ],
     );
--- lib/widgets/my_card_page_view.dart
+++ lib/widgets/my_card_page_view.dart
@@ -3,13 +3,14 @@ import 'package:responsiveui/widgets/my_card.dart';
 import 'package:expandable_page_view/expandable_page_view.dart';
 
 class MyCardPageView extends StatelessWidget {
-  const MyCardPageView({super.key});
+  const MyCardPageView({super.key, required this.pageController});
+  final PageController pageController;
 
   @override
   Widget build(BuildContext context) {
     return ExpandablePageView(
-      
-      scrollDirection: Axis.horizontal,
-      children: List.generate(3, (index)=> MyCard()));
+      controller: pageController,
+        scrollDirection: Axis.horizontal,
+        children: List.generate(3, (index) => MyCard()));
   }
 }
